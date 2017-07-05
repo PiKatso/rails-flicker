905e7d6d338834440df7c64612e4182e978c064b
Kat Gifford and Mohamed Warsame <mohamed@>
2017-07-05 15:59:20 -0700

    add signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  # helper_method :current_user
 
+  # helper_method :current_user
+  #
   # def current_user
   #   if session[:user_id]
   #     @current_user ||= User.find(session[:user_id])

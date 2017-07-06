2c044723f7d5ae47ab71798e9cb6dff2db55689e
Kat Gifford and Mohamed Warsame <kat@>
2017-07-06 14:33:27 -0700

    correct devise update-user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    #  devise_parameter_sanitizer.for(:account_update) {
-    #     |u| u.permit(:username, :email, :password, :current_password, :avatar) }
+     devise_parameter_sanitizer.permit(:account_update, keys: [
+       :username, :email, :password, :current_password, :avatar])
    end
 
 end

481741afbbe1618fc0854ddf489483a19beefcdd
Kat Gifford and Mohamed Warsame <mohamed@>
2017-07-06 08:57:59 -0700

    add fake posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Comment.destroy_all
+
+
+2.times do |i|
+  user = User.create!(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8),
+    admin: false
+  )
+  5.times do |i|
+   post = user.posts.create!(
+     image: "http://www.qygjxz.com/data/out/179/5477898-pics-of-nature.jpg",
+     content: Faker::Friends.quote
+     )
+  #  3.times do |i|
+  #    comment = post.comments.create!(
+  #        content: Faker::TwinPeaks.quote
+  #        )
+  #  end
+  end
+end
+
+p "Created #{Post.count} posts"
+
+# once initially set up/to edit must run 'rake db:setup'
+# also 'rake db:test:prepare'
